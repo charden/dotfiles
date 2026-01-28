7ac086239d8be69c8530fbed021af81e92afc252
charden <licht1017@gmail.com>
2026-01-28 22:18:34 +0900

    ezaのありなしでaliasをわける

--- dot_config/zsh/aliases.zsh
+++ dot_config/zsh/aliases.zsh
@@ -1,3 +1,9 @@
 # eza
-alias ls='eza --icons --git'
-alias ll='eza --icons --git -l'
+if [ -x "`which eza 2>/dev/null`" ]; then
+  alias ls='eza --icons --git'
+  alias ll='eza --icons --git -l'
+  alias la='eza --icons --git -la'
+else
+  alias ll='ls -l'
+  alias la='ls -la'
+fi
